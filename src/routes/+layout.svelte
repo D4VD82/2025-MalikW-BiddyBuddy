674ee398895aad7d88ae00048bfbeec04dc69411
D4VD82 <amowahab09@gmail.com>
2025-10-25 21:01:52 +0000

    Added Nav

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,3 +9,28 @@
 </svelte:head>
 
 {@render children?.()}
+
+<nav class="nav_bar">
+	<a href="/dirty"> Dirty</a>
+	<a href="/annoying"> Annoying</a>
+	<a href="/home"> Home</a>
+	<a href="/hungry"> Hungry</a>
+	<a href="/ill"> Ill</a>
+	<a href="/neglected"> Neglected</a>
+	<a href="/sleepy"> Sleepy</a>
+</nav>
+
+<style>
+.nav_bar{
+	display:flex;
+	justify-content: space-around;
+}
+
+.nav_bar > * {
+	display: flex;
+	justify-content: center;
+	width: 80px;
+	height:20px;
+	border: 2px solid black;
+}
+</style>
---
+++ src/routes/hungry/+page.svelte
@@ -0,0 +1,79 @@
+<div class="bg">
+  <div class="list">
+    <h1>Your Tasks</h1> 
+    <img height="20" src="https://static.vecteezy.com/system/resources/thumbnails/017/172/388/small_2x/warning-message-concept-represented-by-exclamation-mark-icon-exclamation-symbol-in-circle-png.png" alt=""> *Warning Hunger Low
+    <hr>
+    <h2>Today</h2>
+    <ul> 
+    <li style="color: green">Take Out the Trash 5:00PM <img height="20" src="https://www.iconpacks.net/icons/2/free-apple-icon-3155-thumb.png" alt="">  </li>
+    <li style="color: orange">Study 6:00PM - 9:00PM  <img height="20" src="https://cdn-icons-png.flaticon.com/512/4465/4465538.png"alt=""></li>
+  </ul>
+    <hr>
+    <h2>Scheduled</h2>
+    <ul>
+    <li style="color: orange"> Vist my Mom 2/18/21 12:00PM<img height="20" src="https://cdn-icons-png.flaticon.com/512/2331/2331996.png"alt=""></li>
+    <li style="color: red">Finish my Job Application 2/20/21  <img height="20" src="https://cdn-icons-png.flaticon.com/512/8692/8692103.png"alt=""></li>
+  </ul>
+  <hr>
+  <img height="240" width= "240" src="https://media.gcflearnfree.org/content/560be4f6557fa909dc9cf5a1_09_30_2015/googletips_calendar_mini.jpg" alt=""/>
+  </div>
+
+<div class="pet-box">
+  <div class="my-grid">
+  <div class="heading"> 
+    <div class="box2"> <img height="100" src="https://i.imgur.com/tbYi2Rq.png" alt=""/></div> 
+    <div class = "box3"><img height="200" width= "200"src="https://i.imgur.com/38Mdwdh.png" alt="" /></div>
+    </div>
+  <div class="body">
+    <div class="box"> <img height="230" width= "230"src="https://tamagotchi-official.com/tamagotchi/jp/character/2024/05/08/wxPzurvEva3yLjCi/02_%E3%81%BE%E3%82%8B%E3%81%A3%E3%81%A1.png" alt=""/> </div>
+  </div>
+  </div>
+  </div>
+</div>
+<style>
+    .bg {
+  display: flex;
+}
+
+.list {
+  height: 620px;
+  width: 300px;
+  border: 1px solid black;
+  padding: 5px; 
+}
+
+.pet-box {
+  display: flex;
+  height: 620px;
+  width: 700px;
+  border: 1px solid black;
+  padding: 5px;
+  background-image: url(https://gallery.yopriceville.com/var/albums/Backgrounds/Beach_Sand_Background.jpg?m=1629778376);
+  justify-content: center;
+}
+
+.box {
+  display: flex;
+  justify-content: center;
+  align-self: center;
+  margin-top: -70px;
+}
+
+.box2 {
+  display: flex;
+  align-items: flex-start;
+}
+
+.box3 {
+  align-items: flex-start;
+  justify-content: center;
+  margin-top: -30px;
+  margin-left:20px;
+}
+
+.my-grid  {
+  grid-template-rows: 100px 100px;
+  grid-template-columns: 100px 100px 100px 100px 100px;
+}
+
+</style>
